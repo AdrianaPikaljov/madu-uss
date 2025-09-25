659472e42ce88ccd36caef65427a508357bc8f30
AdrianaPikaljov <pikaljovadri@gmail.com>
2025-09-25 17:51:39 +0300

    dfgdgdf

---
+++ BonusFood.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace madu
+{
+
+    class BonusFood : Point
+    {
+        private DateTime createdTime;
+
+        public BonusFood(int x, int y, char sym) : base(x, y, sym)
+        {
+            createdTime = DateTime.Now;
+        }
+
+        public bool IsExpired()
+        {
+            return (DateTime.Now - createdTime).TotalSeconds > 5;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,94 +1,113 @@
 ﻿using System;
 using System.Threading;
+using System.Media;
+using madu;
 
-namespace madu
+Console.SetWindowSize(90, 25);
+Console.SetBufferSize(90, 25);
+Console.CursorVisible = false;
+
+Walls walls = new Walls(80, 25);
+walls.Draw();
+
+// Küsi mängija nime enne mängu alustamist
+Console.SetCursorPosition(0, 0);
+Console.Write("Sisesta nimi: ");
+string playerName = Console.ReadLine();
+Player player = new Player(playerName);
+
+// Tühjendame ülemise rea pärast nime sisestust
+Console.SetCursorPosition(0, 0);
+Console.Write(new string(' ', 80));
+
+// Mängu objektid
+Point p = new Point(4, 5, '*');
+Snake snake = new Snake(p, 4, Direction.Right);
+snake.Draw();
+
+FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+Point food = foodCreator.CreateFood();
+food.Draw();
+
+Score score = new Score();
+Level level = new Level();
+
+bool playing = true;
+while (playing)
 {
-    class Program
+    if (walls.IsHit(snake) || snake.IsHitTail())
     {
-        static void Main(string[] args)
+        playing = false;
+        break;
+    }
+
+    if (snake.Eat(food))
+    {
+        // Replace the problematic code with the following:  
+        //var nam = new SoundPlayer("eat.wav");
+        //nam.Load();
+        //nam.Play();
+        //nam.PlaySync();
+
+        score.EatFood();
+        food = foodCreator.CreateFood();
+        food.Draw();
+
+        if (score.CurrentScore % 5 == 0)
         {
-            Console.SetWindowSize(80, 25);
-            Console.SetBufferSize(80, 25);
-            Console.CursorVisible = false;
-
-            Walls walls = new Walls(80, 25);
-            walls.Draw();
-
-            // начальные параметры
-            Point p = new Point(4, 5, '*');
-            Snake snake = new Snake(p, 4, Direction.Right);
-            snake.Draw();
-
-            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
-            Point food = foodCreator.CreateFood();
-            food.Draw();
-
-            // классы игрока, уровня и очков
-            Console.SetCursorPosition(0, 0);
-            Console.Write("kirjuta nimi: ");
-            string playerName = Console.ReadLine();
-            Player player = new Player(playerName);
-
-            Score score = new Score();
-            Level level = new Level();
-
-            bool playing = true;
-            while (playing)
-            {
-                // проверка на проигрыш
-                if (walls.IsHit(snake) || snake.IsHitTail())
-                {
-                    playing = false;
-                    break;
-                }
-
-                // если змейка съела еду
-                if (snake.Eat(food))
-                {
-                    score.EatFood();
-                    food = foodCreator.CreateFood();
-                    food.Draw();
-
-                    // каждые 5 очков – новый уровень
-                    if (score.CurrentScore % 5 == 0)
-                    {
-                        level.LevelUp();
-                    }
-                }
-                else
-                {
-                    snake.Move();
-                }
-
-                // отображение очков и уровня
-                score.DisplayScore(2, 0);
-                level.DisplayLevel(2, 1);
-
-                Thread.Sleep(level.Speed);
-
-                // управление
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey(true);
-                    snake.HandleKey(key.Key);
-                }
-            }
-
-            // конец игры
-            Console.Clear();
-            Console.WriteLine("=== game over ===");
-            Console.WriteLine($"player: {player.Name}");
-            Console.WriteLine($"score: {score.CurrentScore}");
-
-            // сохранить результат
-            player.SetFinalScore(score.CurrentScore);
-            player.SaveResult();
-
-            Console.WriteLine("\ntabel rekorditega:");
-            Player.DisplayResults();
-
-            Console.WriteLine("\nvajuta suvalist nuppu et valjuda");
-            Console.ReadKey();
+            level.LevelUp();
         }
     }
+    else
+    {
+        snake.Move();
+    }
+
+    // === Kuvame skoori ja mängija nime ülemises reas ===
+    Console.SetCursorPosition(0, 0);
+    Console.Write($"Skoor: {score.CurrentScore}   Tase: {level.CurrentLevel}");
+
+    Console.SetCursorPosition(40, 0);
+    Console.Write($"Mängija: {player.Name}");
+
+    Thread.Sleep(level.Speed);
+
+    if (Console.KeyAvailable)
+    {
+        ConsoleKeyInfo key = Console.ReadKey(true);
+        snake.HandleKey(key.Key);
+    }
 }
+BonusFood bonusFood = new BonusFood(10, 5, '@');
+Console.WriteLine("BonusFood created!");
+
+// Kontrollime iga sekundi tagant, kas bonus toit on aegunud
+while (true)
+{
+    if (bonusFood.IsExpired())
+    {
+        Console.WriteLine("BonusFood is expired.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("BonusFood is still valid.");
+    }
+    System.Threading.Thread.Sleep(1000); // ootab 1 sekundi
+}
+
+
+// Mäng läbi
+Console.Clear();
+Console.WriteLine("=== MÄNG LÄBI ===");
+Console.WriteLine($"Mängija: {player.Name}");
+Console.WriteLine($"Skoor: {score.CurrentScore}");
+
+player.SetFinalScore(score.CurrentScore);
+player.SaveResult();
+
+Console.WriteLine("\nTabel rekorditega:");
+Player.DisplayResults();
+
+Console.WriteLine("\nVajuta suvalist klahvi, et väljuda...");
+Console.ReadKey();
